f48a09429b3efc7e579089fbf671ebb8642bd670
tarek-goher <taroka430@gmail.com>
2025-10-01 06:46:54 +0300

    Fix Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,21 @@ import Home from "./Home";
 import ServicesPage from "./Components/Service/Service";
 import ContactPage from "./Components/Contact/Contact";
 import FAQPage from "./Components/FAQ/FAQ";
-
 import ClientContactForm from "./Components/Template/Template";
 import FormDataReceiver from "./Components/form data/Formdata";
 import AboutUs from "./Components/About/AboutUS";
 import Privacy from "./Components/Policy/Privacy";
-// import NotFound from './Components/NotFound'
-import HomePreview from './Components/HomePreview/HomePreview'
+import HomePreview from './Components/HomePreview/HomePreview';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/CodeSphere"> {/* ← اضفت basename */}
       <Routes>
         <Route path="*" element={<Home />} />
         <Route path="/Service" element={<ServicesPage />} />
         <Route path="/Contact" element={<ContactPage />} />
         <Route path="/AboutUs" element={<AboutUs />} />
-        // <Route path="/FAQ" element={<FAQPage />} />
+        {/* <Route path="/FAQ" element={<FAQPage />} /> */}
         <Route path="/Template" element={<ClientContactForm />} />
         <Route path="/Privacy" element={<Privacy />} />
         <Route path="/Formdata" element={<FormDataReceiver />} />
