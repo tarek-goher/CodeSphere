2fe20fe806238b72e9eaa3b85abe309f4afa2904
tarek-goher <taroka430@gmail.com>
2025-10-01 07:04:26 +0300

    Prepare project for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import ServicesPage from "./Components/Service/Service";
 import ContactPage from "./Components/Contact/Contact";
-import FAQPage from "./Components/FAQ/FAQ";
 import ClientContactForm from "./Components/Template/Template";
 import FormDataReceiver from "./Components/form data/Formdata";
 import AboutUs from "./Components/About/AboutUS";
@@ -11,18 +10,16 @@ import HomePreview from './Components/HomePreview/HomePreview';
 
 function App() {
   return (
-    <Router basename="/CodeSphere"> {/* ← اضفت basename */}
+    <Router basename={process.env.GITHUB_PAGES ? "/CodeSphere" : "/"}>
       <Routes>
         <Route path="*" element={<Home />} />
         <Route path="/Service" element={<ServicesPage />} />
         <Route path="/Contact" element={<ContactPage />} />
         <Route path="/AboutUs" element={<AboutUs />} />
-        {/* <Route path="/FAQ" element={<FAQPage />} /> */}
         <Route path="/Template" element={<ClientContactForm />} />
         <Route path="/Privacy" element={<Privacy />} />
         <Route path="/Formdata" element={<FormDataReceiver />} />
         <Route path="/HomePreview" element={<HomePreview />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
   );
