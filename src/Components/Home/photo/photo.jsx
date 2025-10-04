1cacb2ebea8fb2f40b838056c1398cc7032ccdf5
tarek-goher <taroka430@gmail.com>
2025-10-04 15:53:15 +0300

    Describe the update here

--- src/Components/Home/photo/photo.jsx
+++ src/Components/Home/photo/photo.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import image from '../../../assets/image.png';
-
+import image from '../../../Assets/image.png';
+import './photo.css';
 const FixedImageComponent = () => {
   return (
-    <div className="min-h-screen bg-black">
-      <div className="w-screen h-screen">
-        <div className="w-full h-full">
-          <img
-            src={image}
-            alt="Beautiful landscape"
-            className="w-full h-full object-cover"
-            style={{ display: 'block' }}
-          />
+    <div className=" bg-gradient-to-b from-gray-900 to-black editImage ">
+      <div className="pt-20 ">
+        <div className="flex justify-center items-center px-4 ">
+          <div className="w-full max-w-7xl ">
+            <div className="relative rounded-2xl overflow-hidden shadow-2xl aspect-video bg-black images">
+              <img
+                src={image}
+                alt="Beautiful landscape"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
